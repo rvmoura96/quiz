6300da3a3651ce04403cb838de0ed8bf7439e46d
rvmoura <rvmoura.96@gmail.com>
2019-01-19 15:55:16 -0200

    Adicionando limitador na criação de perguntas e respostas de acordo com as regras especificadas.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Questionario, Pergunta, Resposta
 # Register your models here.
+
+admin.site.register(Questionario)
+admin.site.register(Pergunta)
+admin.site.register(Resposta)
--- core/models.py
+++ core/models.py
@@ -5,13 +5,46 @@ class Questionario(models.Model):
     titulo = models.CharField('Titulo', max_length=255)
     numero_de_respotas = models.PositiveIntegerField(default=0)
 
+    def __str__(self):
+        return self.titulo
+
 
 class Pergunta(models.Model):
     pergunta = models.CharField('Pergunta', max_length=255)
     questionario = models.ForeignKey(Questionario, on_delete=models.CASCADE)
+
+    def clean(self):
+        """Valida o número máximo de perguntas relacionadas a um questionarios.
+
+        Caso o questionario utilizado no relacionamento, tenha um total de
+        perguntas relacionadas a ele >= 10, a instância de pergunta em questão
+        não será salva.
+        """
+        if self.questionario.pergunta_set.count() >= 10:
+            raise Exception(
+                f'{self.questionario.titulo} possuí o limite de 10 perguntas.'
+            )
+
+    def __str__(self):
+        return self.pergunta
 
 
 class Resposta(models.Model):
     resposta = models.CharField('Resposta', max_length=255)
     correta = models.BooleanField(default=False)
     pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
+
+    def clean(self):
+        """Valida o número máximo de respostas relacionadas a uma pergunta.
+
+        Caso a pergunta utilizada no relacionamento, tenha um total de
+        respostas relacionadas a ele >= 4, a instância de resposta em questão
+        não será salva.
+        """
+        if self.pergunta.resposta_set.count() >= 4:
+            raise Exception(
+                f'{self.pergunta.pergunta} possuí o limite de 4 opções.'
+            )
+
+    def __str__(self):
+        return self.resposta
